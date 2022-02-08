9b86322b289ede80c3938bc1915ea1eb1ceb9955
Dylan <dylanjp@iinet.net.au>
2022-02-08 21:55:51 +1100

    made main menu work are you proud of me teach?

--- UI/Play_Button.gd
+++ UI/Play_Button.gd
@@ -9,7 +9,10 @@ extends Button
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
+func _pressed():
+	print("play")
+	get_tree().change_scene("res://MainGame/MainGame.tscn")
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
