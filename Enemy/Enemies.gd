865247cc8f925a85bf57261ffbe93ad13a779265
Zefffie <dylanjp@iinet.net.au>
2022-04-26 14:26:40 +1000

    Made Enemies Move

--- Enemy/Enemies.gd
+++ Enemy/Enemies.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
 
+var speed = 100
+var direction = "right"
+var rng = RandomNumberGenerator.new()
+
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,8 +13,21 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	rng.randomize()
+	set_physics_process(true)
 
+
+
+
+func _physics_process(delta):
+	if direction == "right":
+		global_position.x += speed * delta
+		if global_position.x >= 860:
+			direction = "left"
+	else:
+		global_position.x -= speed * delta
+		if global_position.x <= 650:
+			direction = "right"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
